2dbb04fcc3fc8a5a675e3154a0a87079513c9c32
space <enaenen@naver.com>
2023-05-17 14:47:59 +0900

    Member respository test

--- src/test/java/hello/servlet/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/member/MemberRepositoryTest.java
@@ -6,19 +6,23 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 //junit 5 부터 public 필요 X
 class MemberRepositoryTest {
     // Spring Bean 을 쓰면 싱글톤을 보장해주므로 필요없다.
     MemberRepository memberRepository = MemberRepository.getInstance();
 
     @AfterEach
-    void afterEach(){
+    void afterEach() {
         //테스트 끝난 후 초기화
         memberRepository.clearStore();
     }
 
     @Test
-    void save(){
+    void save() {
         //given
         Member member = new Member("hello", 20);
         //when
@@ -26,7 +30,24 @@ class MemberRepositoryTest {
 
         //then
         Member findMember = memberRepository.findById(savedMember.getId());
-        Assertions.assertThat(findMember).isEqualTo(savedMember);
+        assertThat(findMember).isEqualTo(savedMember);
+
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
 
     }
 
