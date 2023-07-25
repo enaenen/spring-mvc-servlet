4a0ade10deb19caa8dd9e32943a9e931b4066072
Woo Joo Chae <enaenen@naver.com>
2023-07-25 11:02:56 +0900

    -

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -9,17 +9,20 @@ import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
 
+        //요청 값 읽기 hello?username=chae
         String username = request.getParameter("username");
         System.out.println("username = " + username);
 
         response.setContentType("text/plain");
-        response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello " + username); // body - data
+        response.setCharacterEncoding("utf-8");//header 정보에 들어갈 내용
+        response.getWriter().write("hello " + username);
     }
 }
